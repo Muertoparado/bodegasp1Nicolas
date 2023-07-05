dfd8af336e846df421f732ef792ae4f21c12376c
Muertoparado <nicolas.casanova20@gmail.com>
2023-07-05 09:30:04 -0500

    feat: :construction: metodo post pero tiene error al guardar el dato

--- routers/routes.js
+++ routers/routes.js
@@ -1,4 +1,5 @@
 import Router from 'express';
+import { request } from 'http';
 import mysql from 'mysql2';
 const storageBd= Router();
 let con= undefined;
@@ -13,7 +14,18 @@ storageBd.get("/bodegas", (req, res) => {
     con.query(/*sql*/`SELECT * FROM bodegas ORDER BY nombre`,
     (err,data,fil)=>{
         res.send(JSON.stringify(data));
-        res.end(json.stringify(data));
+        res.end(JSON.stringify(data));
+    }
+    )
+})
+storageBd.post("/bodegas/add", (req, res) => {
+   let datos=req.body;
+   // const id=datos.id;
+    con.query(/*sql*/`INSERT INTO bodegas (datos.id, datos.nombre, datos.id_responsable, datos.estado, datos.created_by, datos.update_by, datos.created_at, datos.updated_at, datos.deleted_at)`,
+    (err,data,fil)=>{
+        res.send(JSON.stringify(datos));
+        console.log(datos);
+        res.end(JSON.stringify(data));
     }
     )
 })
